2200e9156a2fbaaab880fca13ffc1a8c50a4f831
aktoriukas <gediminas@aktoriukas.com>
2022-06-23 14:34:21 +0100

    design

--- src/bg.js
+++ src/bg.js
@@ -1,12 +1,12 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status == "complete" && tab.active) {
-    chrome.storage.sync.get("toggleOnDefault", ({ toggleOnDefault }) => {
-      if (!toggleOnDefault) return
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//   if (changeInfo.status == "complete" && tab.active) {
+//     chrome.storage.sync.get("toggleOnDefault", ({ toggleOnDefault }) => {
+//       if (!toggleOnDefault) return
 
-      chrome.scripting.executeScript({
-        target: { tabId, allFrames: true },
-        files: ["src/convert.js"],
-      })
-    })
-  }
-})
+//       chrome.scripting.executeScript({
+//         target: { tabId, allFrames: true },
+//         files: ["src/convert.js"],
+//       })
+//     })
+//   }
+// })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-let btn_active = document.getElementById("btn_active")
+let btn_active = document.getElementById("bionic_reading_btn")
+const readingActive = document.cookie.includes("bionic_reading_active=true")
+if (readingActive) {
+  btn_active.innerText = "Deactivate"
+} else {
+  btn_active.innerText = "Activate"
+}
 
 btn_active.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+  const readingActive = document.cookie.includes("bionic_reading_active=true")
+
+  if (readingActive) {
+    btn_active.innerText = "Activate"
+    document.cookie = "bionic_reading_active=false"
+  } else {
+    btn_active.innerText = "Deactivate"
+    document.cookie = "bionic_reading_active=true"
+  }
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id, allFrames: true },
